5c79ffade63dc9e871fe1ada5419cc57260dcead
chenxiaokunkun <1729803981@qq.com>
2018-05-29 20:48:24 +0800

    mobile

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
