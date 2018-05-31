32671df6c05b96b62420787355882d7b0014e8b3
chenxiaokunkun <1729803981@qq.com>
2018-05-31 18:48:32 +0800

    mobile

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ $(function(){
     var mySwiper = new Swiper ('.swiper-container', {
 
         effect:'coverflow',/*3D流*/
-        slidesPerView:1.5,/*1.5页*/
-        centerdeSlides:true,
-        initialSlide:4,
+        slidesPerView: 1.5,/*展示1.5页*/
+        centeredSlides: true,
+        initialSlide:4,/*跳到第四页*/
 
 
         // 如果需要分页器
